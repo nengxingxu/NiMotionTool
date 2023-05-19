88f7e34ff874d5ff451b141ac4e350f24cef7da9
nengxing_xu <nengxing_xu@foxitsoftware.com>
2023-05-19 20:38:21 +0800

    add cs file.

---
+++ NiMotion/Model/ListDataModel.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+
+namespace NiMotion.Model
+{
+    public class ListDataModel : ViewModelBase
+    {
+        public int Index { get; set; }
+
+        //public string Name { get; set; }
+        private string name;
+        public string Name
+        {
+            get => name;
+            set => Set(ref name, value);
+        }
+
+
+        public bool IsSelected { get; set; }
+
+        public string Path { get; set; }
+
+        public string ImgPath { get; set; }
+
+        public ObservableCollection<ListDataModel> DataList { get; set; }
+
+
+    }
+}
